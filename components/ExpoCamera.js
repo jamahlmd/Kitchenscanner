0ad6d65e20cb143b3566386368b66b7f3db40f6f
jamahlmd <jamahlmd@gmail.com>
2018-10-03 17:01:58 +0200

    API scan af

--- components/ExpoCamera.js
+++ components/ExpoCamera.js
@@ -13,6 +13,11 @@ class ExpoCamera extends React.Component {
         caption: ''
     };
 
+
+    componentDidUpdate(prevState, prevProps) {
+
+    }
+
     async componentWillMount() {
         const { status } = await Permissions.askAsync(Permissions.CAMERA);
         this.setState({ hasCameraPermission: status === 'granted' });
@@ -24,15 +29,15 @@ class ExpoCamera extends React.Component {
 
     handleFetchData = () => {
 
-        const data = this.state.uri;
+        const data = Base64Binary.decode(this.state.uri);
 
 
 
         fetch(this.state.uriBase, {
             method: 'POST',
-            body: JSON.stringify(data),
+            body: data,
             headers:{
-                'Content-Type': 'application/octet-stream',//multipart/form-data - application/octet-stream
+                'Content-Type': 'application/octet-stream',  //multipart/form-data - application/octet-stream
                 'Ocp-Apim-Subscription-Key': this.state.key
             }
         }).then(res => res.json())
@@ -48,8 +53,8 @@ class ExpoCamera extends React.Component {
             .catch(error => console.log('Error:', error));
     };
 
-    onPictureSaved = ({uri}) => {
-        this.setState({uri}, () => {
+    onPictureSaved = ({base64}) => {
+        this.setState({uri: base64}, () => {
             this.handleFetchData();
         });
     };
@@ -57,7 +62,7 @@ class ExpoCamera extends React.Component {
 
     snap = async () => {
         if (this.camera) {
-            let photo = await this.camera.takePictureAsync().then((data) => this.onPictureSaved(data));
+            let photo = await this.camera.takePictureAsync({base64: true}).then((data) => this.onPictureSaved(data));
         }
     };
 
@@ -99,5 +104,66 @@ class ExpoCamera extends React.Component {
 ExpoCamera.navigationOptions = {
     header: null
 };
+
+
+var Base64Binary = {
+    _keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
+
+    /* will return a  Uint8Array type */
+    decodeArrayBuffer: function(input) {
+        var bytes = (input.length/4) * 3;
+        var ab = new ArrayBuffer(bytes);
+        this.decode(input, ab);
+
+        return ab;
+    },
+
+    removePaddingChars: function(input){
+        var lkey = this._keyStr.indexOf(input.charAt(input.length - 1));
+        if(lkey == 64){
+            return input.substring(0,input.length - 1);
+        }
+        return input;
+    },
+
+    decode: function (input, arrayBuffer) {
+        //get last chars to see if are valid
+        input = this.removePaddingChars(input);
+        input = this.removePaddingChars(input);
+
+        var bytes = parseInt((input.length / 4) * 3, 10);
+
+        var uarray;
+        var chr1, chr2, chr3;
+        var enc1, enc2, enc3, enc4;
+        var i = 0;
+        var j = 0;
+
+        if (arrayBuffer)
+            uarray = new Uint8Array(arrayBuffer);
+        else
+            uarray = new Uint8Array(bytes);
+
+        input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+        for (i=0; i<bytes; i+=3) {
+            //get the 3 octects in 4 ascii chars
+            enc1 = this._keyStr.indexOf(input.charAt(j++));
+            enc2 = this._keyStr.indexOf(input.charAt(j++));
+            enc3 = this._keyStr.indexOf(input.charAt(j++));
+            enc4 = this._keyStr.indexOf(input.charAt(j++));
+
+            chr1 = (enc1 << 2) | (enc2 >> 4);
+            chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+            chr3 = ((enc3 & 3) << 6) | enc4;
+
+            uarray[i] = chr1;
+            if (enc3 != 64) uarray[i+1] = chr2;
+            if (enc4 != 64) uarray[i+2] = chr3;
+        }
+
+        return uarray;
+    }
+}
 
 export default ExpoCamera;
